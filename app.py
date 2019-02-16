152c7ce808ef0965f3c34531c802aa08f762e54f
Pulkit Mishra <pulkitmishra007@gmail.com>
2019-02-16 20:07:11 +0530

    No! Wait THIS IS THE FIRST COMMIT (#2)

--- app.py
+++ app.py
@@ -48,3 +48,42 @@ def process_message(message, lower_case = True, stem = True, stop_words = True, 
         stemmer = PorterStemmer()
         words = [stemmer.stem(word) for word in words]
     return words
+
+class TweetClassifier(object):
+    def __init__(self, trainData, method = 'tf-idf'):
+        self.tweets, self.labels = trainData['message'], trainData['label']
+        self.method = method
+
+    def train(self):
+        self.calc_TF_and_IDF()
+        self.calc_TF_IDF()
+
+    def calc_TF_and_IDF(self):
+        noOfMessages = self.tweets.shape[0]
+        self.depressive_tweets, self.positive_tweets = self.labels.value_counts()[1], self.labels.value_counts()[0]
+        self.total_tweets = self.depressive_tweets + self.positive_tweets
+        self.depressive_words = 0
+        self.positive_words = 0
+        self.tf_depressive = dict()
+        self.tf_positive = dict()
+        self.idf_depressive = dict()
+        self.idf_positive = dict()
+        for i in range(noOfMessages):
+            message_processed = process_message(self.tweets.iloc[i])
+            count = list() #To keep track of whether the word has ocured in the message or not.
+                           #For IDF
+            for word in message_processed:
+                if self.labels.iloc[i]:
+                    self.tf_depressive[word] = self.tf_depressive.get(word, 0) + 1
+                    self.depressive_words += 1
+                else:
+                    self.tf_positive[word] = self.tf_positive.get(word, 0) + 1
+                    self.positive_words += 1
+                if word not in count:
+                    count += [word]
+            for word in count:
+                if self.labels.iloc[i]:
+                    self.idf_depressive[word] = self.idf_depressive.get(word, 0) + 1
+                else:
+                    self.idf_positive[word] = self.idf_positive.get(word, 0) + 1
+
