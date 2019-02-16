a39e00367329c6c1693a89f7736ac5031fef6d24
Pulkit Mishra <pulkitmishra007@gmail.com>
2019-02-16 19:09:52 +0530

    Basic Fixes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
 from math import log, sqrt
 import pandas as pd
 import numpy as np
