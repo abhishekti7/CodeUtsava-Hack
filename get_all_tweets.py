727c35c5997c280fca426eb5d821e8c34a500c09
Pulkit Mishra <pulkitmishra007@gmail.com>
2019-02-17 08:08:45 +0530

    Changes to Scraping module

--- get_all_tweets.py
+++ get_all_tweets.py
@@ -1,9 +1,11 @@
 import twint
+import os
 def get_all_tweets(username):
-	# Set up TWINT config
+	if os.path.isfile('dataset/tweets.csv'):
+		os.remove('dataset/tweets.csv')
 	c = twint.Config()
 	c.Username = username
-	c.Store_json = True
+	c.Store_csv = True
 	c.Custom["Tweet"] = ["tweet"]
 	c.Custom["Date"] = ["date"]
 	c.Custom["Time"] = ["time"]
